610df16cc9193f1cc659ab61c6484a253ec225c1
PaulE <paul@PaulEs-MacBook-Pro.local>
2014-02-08 21:36:37 -0600

    finished step10-rest

---
+++ app/js/services.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* Services 
+This service allows you have to have more RESTful API for your 
+controllers.
+*/
+
+
+var phonecatServices = angular.module('phonecatServices', ['ngResource']);
+
+phonecatServices.factory('Phone', ['$resource',
+  function($resource){
+    return $resource('phones/:phoneId.json', {}, {
+      query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+    });
+  }]);
