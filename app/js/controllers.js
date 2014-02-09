48c7ce53f12debe2ec67e4bd37f915fb89257456
PaulE <paul@PaulEs-MacBook-Pro.local>
2014-02-08 19:20:59 -0600

    finished step3-databinding

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,14 +1,21 @@
-// http://docs.angularjs.org/tutorial/step_02 
+'use strict';
+
+/* Controllers */
 
 var phonecatApp = angular.module('phonecatApp', []);
- 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
+
+phonecatApp.controller('PhoneListCtrl', function($scope) {
   $scope.phones = [
     {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
+     'snippet': 'Fast just got faster with Nexus S.',
+     'age': 1},
     {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 2},
     {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 3}
   ];
+
+  $scope.orderProp = 'age';
 });
