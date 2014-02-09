1f03b503095cea6c7b1066c83bac9acba7586092
PaulE <paul@PaulEs-MacBook-Pro.local>
2014-02-08 20:20:21 -0600

    finished step6-routes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,20 +1,26 @@
 'use strict';
 
 /* Controllers */
-
-var phonecatApp = angular.module('phonecatApp', []);
-
 /*
  To use $http, you must run:
    node scripts/web-server.js
  and goto:
   localhost:8000/apps/index.html
 */
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
-  $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
 
-  $scope.orderProp = 'age';
-});
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+    function($scope, $http) {
+        $http.get('phones/phones.json').success(function(data) {
+            $scope.phones = data;
+        });
+        $scope.orderProp = 'age';
+
+}]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+    function($scope, $routeParams) {
+        $scope.phoneId = $routeParams.phoneId;
+        
+}]);
